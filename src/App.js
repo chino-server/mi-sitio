bda8979c7a40a2b143a3a64826bc27468d067bed
chino-server <arandapoltuc@gmail.com>
2022-08-25 23:54:40 -0300

    desafio saludo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import ResponsiveAppBar from './Components/Nav/navBar';
+import ItemListContainer from './Components/itemListContainer/itemListContainer';
+
 
 class App extends React.Component {
   render(){
     return (
       <div> 
        <ResponsiveAppBar> </ResponsiveAppBar>
+        <ItemListContainer
+        greeting="greeting"/>
       </div>
     )
   }
---
+++ src/Components/itemListContainer/itemListContainer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ItemListContainer = ({greeting}) => {
+  return (
+    <div>itemListContainer {greeting}</div>
+  )
+}
+
+export default ItemListContainer
